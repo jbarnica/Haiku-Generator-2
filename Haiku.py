2334bb7406bc3e78cab7ba74e23a0e83714dad4d
Ian Dobson <theonegod@gmail.com>
2018-09-14 21:38:18 -0600

    Remove duplicate words.

--- Haiku.py
+++ Haiku.py
@@ -26,7 +26,7 @@ def generate_valid_words():
 def strip_punctuation(phrase):
     for i in list(string.punctuation):
         phrase = phrase.replace(i, '')
-    return phrase
+    return phrase.lower()
 
 def nsyl(word, cmudict):
         return [len(list(y for y in x if y[-1].isdigit())) for x in cmudict[word.lower()]]
@@ -65,18 +65,22 @@ def load_dictionary(phrase=None):
 
 def get_words(number, chosenWords):
     return dict((key, value) for key, value in syllable_dict.items() 
-        if value <= number and value not in (chosenWords or []))
+        if value <= number and key not in (chosenWords or ''))
 
-def choose_words(number, chosenWords = None): 
+def choose_words(number, chosenWords = []): 
     s = []   
     goodWords = get_words(number, chosenWords)
-    choice = random.choice(list(goodWords.keys()))
-    result = (choice, goodWords[choice])
-    s += [result[0]]
-    if result[1] < number:
-        s += choose_words(number - result[1], s)
-    random.shuffle(s)
-    return s
+    if len(goodWords) > 0:
+        choice = random.choice(list(goodWords.keys()))
+        result = (choice, goodWords[choice])
+        s += [result[0]]
+        chosenWords.append(result[0])
+        if result[1] < number:
+            s += choose_words(number - result[1], chosenWords)
+        random.shuffle(s)
+        return s
+    else:
+        return ''
 
 def choose_line(number):
     elements = choose_words(number)
@@ -84,7 +88,7 @@ def choose_line(number):
     return '{}\n'.format(' '.join(elements))
 
 def write_haiku():
-    haiku = "{}{}{}".format(choose_line(5), choose_line(7), choose_line(5))
+    haiku = '"{}{}{}"'.format(choose_line(5), choose_line(7), choose_line(5))
     return haiku
 
 def write_undefined(syllabelCounts):
--- Twitter.py
+++ Twitter.py
@@ -2,7 +2,7 @@ import configparser
 import Haiku
 import twitter
 
-TWITTER_HANDLE
+TWITTER_HANDLE = ''
 
 def user_tweet(num, thandle):
     statuses = api.GetUserTimeline(screen_name=thandle)
@@ -17,14 +17,14 @@ def setup():
     config = configparser.ConfigParser()
     config.read('setup.ini')
 
-    TWITTER_HANDLE = config.get('Information', 'consumer_key', raw=False)
+    TWITTER_HANDLE = config.get('Information', 'handle', raw=False)
 
     api = twitter.Api(
         consumer_key=config.get('Information', 'consumer_key', raw=False),
         consumer_secret=config.get('Information', 'consumer_secret', raw=False),
         access_token_key=config.get('Information', 'access_token_key', raw=False),
         access_token_secret=config.get('Information', 'access_token_secret', raw=False))        
-    return api        
+    return api
 
 def get_latest_tweet():
     return user_tweet(1, TWITTER_HANDLE)
